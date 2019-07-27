1ea77ae1e826b9864b548b64a9673d5d45ba8133
cyanpen <cyanpen13@gmail.com>
2019-07-27 22:45:47 +0900

    19/7/27 now magenta is running!

--- Magenta.php
+++ Magenta.php
@@ -9,13 +9,30 @@ use MagentaTelegram\BotToken;
 
 class Magenta{
 	
-	public $aurl = "https://api.telegram.org/bot";
+	public static $aurl = "https://api.telegram.org/bot";
 	//latest updated id
-	public $luid = "";
+	public static $luid = "";
+	public static $wait = false;
 	
-	public function sendMessage( $chatid, $text ){
+	public static function sendMessage( $chatid, $text, $upid ){
 		
-		$url = $this->aurl.BotToken::getToken()."/sendMessage?chat_id=".$chatid."&text=".urlencode($text);
+		if ( $text == "Magenta의 종료가 취소되었습니다." ){
+			
+			echo "\nAre you sure to off Magenta?\n";
+			
+			$close = "";
+			
+			fscanf( STDIN, "%s\n", $close );
+			
+			if ( $close == "yes" ){
+			
+			die();
+			
+		}
+		
+		}
+		
+		$url = self::$aurl.BotToken::getToken()."/sendMessage?chat_id=".$chatid."&text=".urlencode($text);
 		
 		$curl = curl_init();
 		
@@ -24,36 +41,127 @@ class Magenta{
 		
 		curl_exec($curl);
 		
-		echo "Sending Message to {$chatid}\n".Utils::getTime("Asia/Seoul", "Y-m-d h:i:s")."\n";
+		self::$luid = $upid;
+		
+		echo "Sending Message to {$chatid}\n".Utils::getTime("Asia/Seoul", "Y-m-d h:i:s")."\n\n";
 		
 	}		
 	
-	public function checkUpdate( $isLoop ){
+	public static function checkUpdate( $offset = -1 ){
 		
-		$ud = json_decode ();
-		$sender_name = $ud["result"];
-		$isbot = $ud["result"]["message"]["from"]["is_bot"];
+		$url = self::$aurl.BotToken::getToken()."/getUpdates?offset=".$offset;
+		
+		$curl = curl_init();
+		
+		curl_setopt ( $curl, CURLOPT_URL, $url );
+		curl_setopt ( $curl, CURLOPT_RETURNTRANSFER, true );
+		
+		$json = curl_exec($curl);
+		
+		$arr = json_decode($json, true);
+		
+		//json to array interpretation
+		//print_r ($arr);
+		
+		$rarr = [];
+		
+		//cases
+		 
+		if ( null !== @$arr["result"][0]["channel_post"]["chat"]["type"] ){
+			
+			//echo $arr["result"][0]["channel_post"]["chat"]["type"];
+			
+			if ( @$arr["result"][0]["channel_post"]["chat"]["type"] == "channel" ){
+
+			$rarr["upid"] = $arr["result"][0]["update_id"];
+			$rarr["chat_id"] = $arr["result"][0]["channel_post"]["chat"]["id"];
+			$rarr["rawtitle"] = $arr["result"][0]["channel_post"]["chat"]["title"];
+			$rarr["name"] = $arr["result"][0]["channel_post"]["chat"]["username"];
+			$rarr["type"] = $arr["result"][0]["channel_post"]["chat"]["type"];
+			$rarr["rawtext"] = $arr["result"][0]["channel_post"]["text"];
+			
+			} 
+			
+		}
+				
+		if ( null !== @$arr["result"][0]["message"]["chat"]["type"] ) {
+			
+			if ( @$arr["result"][0]["message"]["chat"]["type"] == "supergroup" ){
+			
+			$rarr["upid"] = $arr["result"][0]["update_id"];
+			$rarr["is_bot"] = $arr["result"][0]["message"]["from"]["is_bot"];
+			$rarr["rawname"] = $arr["result"][0]["message"]["from"]["first_name"];
+			$rarr["user_id"] = $arr["result"][0]["message"]["from"]["username"];
+			$rarr["chat_id"] = $arr["result"][0]["message"]["chat"]["id"];
+			$rarr["rawtitle"] = $arr["result"][0]["message"]["chat"]["title"];
+			$rarr["type"] = $arr["result"][0]["message"]["chat"]["type"];
+			$rarr["text"] = $arr["result"][0]["message"]["text"];
+			$rarr["texttype"] = $arr["result"][0]["message"]["entities"][0]["type"];
+			$rarr["lang"] = $arr["result"][0]["message"]["from"]["language_code"];
+			
+			}
+			
+		}
+			
+		if ( null !== @$arr["result"][0]["message"]["chat"]["type"] ) {	
+			
+			if ( @$arr["result"][0]["message"]["chat"]["type"] == "private" ){
+			
+			$rarr["upid"] = $arr["result"][0]["update_id"];
+			$rarr["is_bot"] = $arr["result"][0]["message"]["from"]["is_bot"];
+			$rarr["rawname"] = $arr["result"][0]["message"]["from"]["first_name"];
+			$rarr["user_id"] = $arr["result"][0]["message"]["from"]["username"];
+			$rarr["chat_id"] = $arr["result"][0]["message"]["chat"]["id"];
+			$rarr["type"] = $arr["result"][0]["message"]["chat"]["type"];
+			$rarr["text"] = $arr["result"][0]["message"]["text"];
+			$rarr["texttype"] = ( @$arr["result"][0]["message"]["entities"][0]["type"] !== null )? $arr["result"][0]["message"]["entities"][0]["type"] : null;
+			$rarr["lang"] = $arr["result"][0]["message"]["from"]["language_code"];
+			
+			}
+			
+		}
+			
+		else{
+			
+			$rarr = [
+			
+			"error" => "undefined type of chat"
+			
+			];
+			
+		}
+		
+		//interpretation function
+		//print_r($rarr);
+		//echo "\n".@$arr["result"][0]["message"]["chat"]["type"]."\n";
+		
+		return $rarr;
 		
 	}
 	
-	public function response( $command, $adres = "" ){
+	public static function response( $command, $darr ){
 		
 		$srd = ( Utils::getTime("Asia/Seoul", "H") >= explode( ":", Utils::getSunrise("INCHEON"))[0] )? "내일":"오늘";
+		$name = ( $darr["type"] == "channel" )? "this function only working on supergroup and private" : $darr["rawname"];
+		$title = ( $darr["type"] == "private" )? "Magenta" : $darr["rawtitle"];
+		$lang = ( $darr["type"] == "channel" )? "this function only working on supergroup and private" : $darr["lang"];
 		
 		$rarr = [
 		
 			"/sunrise" => $srd."의 일출 시각은 ".Utils::getSunrise("INCHEON")." 입니다.",
 			"/time" => "현재 시각은 ".Utils::getTime("Asia/Seoul", "Y-m-d h:i:s")." 입니다.",
-			"/roominfo" => "개발중입니다.",
-			"/userinfo" => ""
+			"/roominfo" => "채팅방명 : {$title}\n채팅방의 유형 : {$darr["type"]}",
+			"/userinfo" => "마젠타가 인지하는 정보를 표시합니다.\n"."유저명 : {$name}\n유저호출명 : @{$darr["user_id"]}\n사용 언어 : {$lang}",
+			"/off" => ( $darr["user_id"] == "CYANPEN" )? "Magenta의 종료가 취소되었습니다." : "관리자만 실행할 수 있습니다."
+			
 		];
 		
 		if ( isset ( $rarr[ $command ] ) ){
-			
+
 			return $rarr[ $command ];
 			
 		} else {
-			
+
 			return "마젠타에 등록되지 않은 명령어입니다.";
 			
 		}
@@ -62,14 +170,47 @@ class Magenta{
 	
 	public static function Run(){
 		
-
+		$arr = self::checkUpdate();
+		
+		if ( @$arr["upid"] !== self::$luid ) {
+		
+		if ( isset ( $arr["texttype"] ) ) {
+		
+		if ( $arr["texttype"] == "bot_command" ){
+			
+			//echo str_replace( "@magenta_bot", "", $arr[7]);
+			
+			self::sendMessage( $arr["chat_id"], self::response( str_replace( "@magenta_bot", "", $arr["text"]), $arr ), $arr["upid"] );
+			
+		} 
+		
+		}
+		
+		} else {
+			
+			if ( self::$wait == false ){
+			
+			echo "\nMagenta is waiting..\n\n";
+			sleep ( 1 );
+			
+			}
+			
+		}
+		
+	}
+	
+	public function __destruct(){
+		
+		Utils::setLuid( self::$luid );
 		
 	}
 
 }
 
-//Magenta::Run();
-$a = new Magenta;
-$a-> sendMessage ( "", Utils::conv_utf8("\ub3d9\ub355 \uc124\ucc0c") );
-echo Utils::conv_utf8("\uc548\ub155\n\ub9c8\uc820\ud0c0");
+for ( $i = 0 ; ; $i++ ){
+
+Magenta::Run();
+
+}
+
 ?>
