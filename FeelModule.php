47fbb974dab0a61be01fdeaafb50b8051fd6d985
cyanpen <cyanpen13@gmail.com>
2019-07-28 22:16:42 +0900

    WIP : expEmotion

---
+++ FeelModule.php
@@ -0,0 +1,30 @@
+<?php
+namespace MagentaTelegram;
+
+include_once "c:/MagentaTelegram/Utils.php";
+
+use MagentaTelegram\Utils;
+
+class Feel{
+	
+	private $feeling = [
+	
+		"joy" => 0,
+		"sad" => 0,
+		"anger" => 0,
+		"fear" => 0,
+		"disgust" => 0,
+		"contempt" => 0,
+		"surprise" => 0
+	
+	];
+
+	public static function expEmotion(){
+		
+		
+		
+	}
+	
+}
+
+?>
